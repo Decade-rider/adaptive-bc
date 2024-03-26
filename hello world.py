72df0ae227b12785597a81e927641646b3ed1b93
Kamenrider <1161949421@qq.com>
2024-03-26 16:24:28 +0800

    hello world.py

---
+++ hello world.py
@@ -0,0 +1,11 @@
+'''
+Author: Kamenrider 1161949421@qq.com
+Date: 2024-03-26 16:24:11
+LastEditors: Kamenrider 1161949421@qq.com
+LastEditTime: 2024-03-26 16:25:21
+FilePath: \adaptive-bc\hello world.py
+Description: 
+
+Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+'''
+print('Hello Wrrld')
